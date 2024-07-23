95494a804fdb1490fe3b521e6e1115c9f085debd
Evandro Calado <evandrocalado07@gmail.com>
2024-07-23 18:37:43 -0300

    refactor: add bg color on stories

--- src/components/Heading/Heading.stories.tsx
+++ src/components/Heading/Heading.stories.tsx
@@ -7,10 +7,24 @@ export default {
   args: {
     children: 'Most Populars',
     color: 'white',
+    lineLeft: false,
+    lineBottom: false,
   },
   argTypes: {
     children: { type: 'string' },
+    color: {
+      options: ['white', 'black'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+    lineLeft: { control: { type: 'boolean' } },
+    lineBottom: { control: { type: 'boolean' } },
   },
 } as Meta;
 
-export const Default: StoryFn<HeadingProps> = (args) => <Heading {...args} />;
+export const Default: StoryFn<HeadingProps> = (args) => (
+  <div className="flex h-28 w-full items-center justify-center bg-gray">
+    <Heading {...args} />
+  </div>
+);
--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -11,4 +11,8 @@ export default {
   component: Logo,
 } as Meta;
 
-export const Default: StoryFn<LogoProps> = (args) => <Logo {...args} />;
+export const Default: StoryFn<LogoProps> = (args) => (
+  <div className="flex h-28 w-full items-center justify-center bg-gray">
+    <Logo {...args} />
+  </div>
+);
