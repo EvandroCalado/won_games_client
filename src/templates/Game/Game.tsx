b45e7b6167e4da64f502bb7336fe8f295ddac63f
Evandro Calado <evandrocalado07@gmail.com>
2024-08-09 21:25:08 -0300

    🐳 chore: add separator

--- src/templates/Game/Game.tsx
+++ src/templates/Game/Game.tsx
@@ -7,6 +7,7 @@ import {
   GameInfo,
   GameInfoProps,
   HighlightProps,
+  Separator,
   Showcase,
   TextContent,
 } from '@/components';
@@ -44,22 +45,30 @@ export const Game: FC<GameProps> = ({
         style={{ backgroundImage: `url(${cover})` }}
         className="absolute top-0 z-[-1] h-96 w-full bg-cover bg-top bg-no-repeat opacity-[0.4] [clip-path:_polygon(0_0,100%_0,100%_100%,0_85%)] md:h-[700px]"
       />
+
       {/* game info  */}
       <Container className="mt-20 md:mt-80">
         <GameInfo {...gameInfo} />
       </Container>
+
       {/* gallery */}
       <Container className="max-md:hidden">
         {!!gallery && <Gallery items={gallery} />}
       </Container>
+
       {/* text content */}
       <Container>
         <TextContent title="Cyberpunk 2077" content={description} />
       </Container>
+
       {/* game details */}
       <Container>
         <GameDetails {...details} />
       </Container>
+
+      {/* separator */}
+      <Separator />
+
       {/* showcases */}
       <Showcase
         title="Upcoming"
