c1211e6fa07dab727c60a6156af1c1828365ddf8
Evandro Calado <evandrocalado07@gmail.com>
2024-08-15 20:07:36 -0300

    🌈 style: add spacings

--- src/templates/Cart/Cart.tsx
+++ src/templates/Cart/Cart.tsx
@@ -34,12 +34,12 @@ export const Cart: FC<CartProps> = ({
 
   return (
     <Base>
-      <Container>
+      <Container className="space-y-8">
         <Heading lineLeft>My cart</Heading>
 
         {/* cart items and payment or empty */}
         {items.length ? (
-          <div className="my-8 grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:[&>:first-child]:col-span-2">
+          <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:[&>:first-child]:col-span-2">
             <CartList items={items} total={total} />
             <PaymentOptions cards={cards} handlePayment={handlePayment} />
           </div>
