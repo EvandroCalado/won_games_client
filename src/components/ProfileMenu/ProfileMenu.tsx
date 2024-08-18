1cf4036438f4d65cca1e9f7e02393d1f9877056f
Evandro Calado <evandrocalado07@gmail.com>
2024-08-18 20:06:44 -0300

    🌈 style: add border

--- src/components/ProfileMenu/ProfileMenu.tsx
+++ src/components/ProfileMenu/ProfileMenu.tsx
@@ -21,21 +21,21 @@ export const ProfileMenu = ({
   <nav className="flex flex-row bg-white text-black md:flex-col md:border-none">
     <Link
       href="/profile/me"
-      className={`${activeLink === '/profile/me' ? 'bg-primary text-white' : ''} flex w-full items-center gap-4 p-4 duration-150 hover:bg-primary hover:text-white max-md:justify-center`}
+      className={`${activeLink === '/profile/me' ? 'bg-primary text-white' : ''} flex w-full items-center gap-4 border-white p-4 duration-150 hover:bg-primary hover:text-white max-md:justify-center max-md:border-r-[1px] md:border-b-[1px]`}
     >
       <IconUserCircle role="img" />
       <span className="max-md:hidden">My profile</span>
     </Link>
     <Link
       href="/profile/cards"
-      className={`${activeLink === '/profile/cards' ? 'bg-primary text-white' : ''} flex w-full items-center gap-4 p-4 duration-150 hover:bg-primary hover:text-white max-md:justify-center`}
+      className={`${activeLink === '/profile/cards' ? 'bg-primary text-white' : ''} flex w-full items-center gap-4 border-white p-4 duration-150 hover:bg-primary hover:text-white max-md:justify-center max-md:border-r-[1px] md:border-b-[1px]`}
     >
       <IconCreditCard role="img" />
       <span className="max-md:hidden">My cards</span>
     </Link>
     <Link
       href="/profile/orders"
-      className={`${activeLink === '/profile/orders' ? 'bg-primary text-white' : ''} flex w-full items-center gap-4 p-4 duration-150 hover:bg-primary hover:text-white max-md:justify-center`}
+      className={`${activeLink === '/profile/orders' ? 'bg-primary text-white' : ''} flex w-full items-center gap-4 border-white p-4 duration-150 hover:bg-primary hover:text-white max-md:justify-center max-md:border-r-[1px] md:border-b-[1px]`}
     >
       <IconList role="img" />
       <span className="max-md:hidden">My orders</span>
