f0b4da1fefd51aa2d60500d086ff3d19474db170
Evandro Calado <evandrocalado07@gmail.com>
2024-08-17 22:38:46 -0300

    ✨ feat: create page profile/me

---
+++ src/app/profile/me/page.tsx
@@ -0,0 +1,12 @@
+import { FormProfile } from '@/components';
+import { Profile } from '@/templates';
+
+const MePage = () => {
+  return (
+    <Profile>
+      <FormProfile />
+    </Profile>
+  );
+};
+
+export default MePage;
