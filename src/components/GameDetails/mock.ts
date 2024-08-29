edce3dddc61aeccbef9964c584fc587bacecd607
Evandro Calado <evandrocalado07@gmail.com>
2024-08-29 09:04:42 -0300

    🐞 fix: change mac for osx

--- src/components/GameDetails/mock.ts
+++ src/components/GameDetails/mock.ts
@@ -3,7 +3,7 @@ import { GameDetailsProps } from './GameDetails';
 export const mock: GameDetailsProps = {
   developer: 'Rockstar Games',
   releaseDate: '2024-11-21T18:30:00',
-  platforms: ['windows', 'mac', 'linux'],
+  platforms: ['windows', 'osx', 'linux'],
   publisher: 'Rockstar Games',
   rating: 'BR0',
   genres: ['Action'],
