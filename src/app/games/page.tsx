099042379b40f198568483ceaff6761b9ba37ed3
Evandro Calado <evandrocalado07@gmail.com>
2024-08-29 09:10:49 -0300

    🐳 chore: add games data dynamic

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,9 +1,25 @@
 import { mock as mockSidebar } from '@/components/ExploreSidebar/mock';
-import { mock as mockGames } from '@/components/GameCardSlider/mock';
+import { GET_GAMES } from '@/graphql/queries';
+import { getClient } from '@/lib/client';
 import { Games } from '@/templates';
 
-const GamesPage = () => {
-  return <Games games={mockGames} filterItems={mockSidebar} />;
+const GamesPage = async () => {
+  const { data } = await getClient().query({
+    query: GET_GAMES,
+    variables: { limit: 9 },
+  });
+
+  const games = data.games.data.map((game) => ({
+    id: game.id,
+    title: game.attributes.name,
+    slug: game.attributes.slug,
+    developer: game.attributes.developers.data[0].attributes.name,
+    img: game.attributes.cover.data?.attributes.formats.small.url,
+    price: game.attributes.price,
+    promotionalPrice: game.attributes.price * 0.75,
+  }));
+
+  return <Games games={games} filterItems={mockSidebar} />;
 };
 
 export default GamesPage;
