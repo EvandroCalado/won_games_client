bf489871aa659b7686fa1c997bc996f240a979d0
Evandro Calado <evandrocalado07@gmail.com>
2024-09-03 08:25:02 -0300

    🐳 chore: add home data and mapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import { mock as banners } from '@/components/BannerSlider/mock';
+import { RibbonColor, RibbonSize } from '@/components';
 import { mock as Slides } from '@/components/GameCardSlider/mock';
 import { mock as Highlight } from '@/components/Highlight/mock';
+import { GET_HOME } from '@/graphql/queries';
+import { getClient } from '@/lib/client';
 import { Home } from '@/templates';
 
-const HomePage = () => {
+const HomePage = async () => {
+  const { data } = await getClient().query({
+    query: GET_HOME,
+  });
+
+  const banners = data.banners.data.map((banner) => ({
+    img: banner.attributes.image.data.attributes.url,
+    title: banner.attributes.title,
+    subtitle: banner.attributes.subtitle,
+    buttonLabel: banner.attributes.button?.label,
+    buttonLink: banner.attributes.button?.link,
+    ribbon: banner.attributes.ribbon?.text,
+    ribbonColor: banner.attributes.ribbon?.color as RibbonColor,
+    ribbonSize: banner.attributes.ribbon?.size as RibbonSize,
+  }));
+
   return (
     <Home
       banners={banners}
