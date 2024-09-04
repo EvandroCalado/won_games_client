ade80737f5c79b3d05c1f2b947f6cb54708c2aa8
Evandro Calado <evandrocalado07@gmail.com>
2024-09-04 20:29:25 -0300

    🐳 chore: update query

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -1,5 +1,11 @@
 import { TypeHome } from '@/types';
 import { gql, TypedDocumentNode } from '@apollo/client';
+import {
+  BannerFragment,
+  GameFragment,
+  HighlightFragment,
+  PaginationFragment,
+} from '../fragments';
 
 export const GET_HOME: TypedDocumentNode<TypeHome> = gql`
   query QueryHome {
@@ -7,30 +13,115 @@ export const GET_HOME: TypedDocumentNode<TypeHome> = gql`
       data {
         id
         attributes {
-          image {
-            data {
-              id
-              attributes {
-                url
-                formats
+          ...BannerFragment
+        }
+      }
+    }
+
+    newGames: games(
+      filters: { release_date: { lte: "2024-09-02" } }
+      sort: "release_date:desc"
+      pagination: { limit: 8 }
+    ) {
+      data {
+        id
+        attributes {
+          ...GameFragment
+        }
+      }
+      meta {
+        pagination {
+          ...PaginationFragment
+        }
+      }
+    }
+
+    upcomingGames: games(
+      filters: { release_date: { gt: "2024-09-02" } }
+      sort: "release_date:asc"
+      pagination: { limit: 8 }
+    ) {
+      data {
+        id
+        attributes {
+          ...GameFragment
+        }
+      }
+      meta {
+        pagination {
+          ...PaginationFragment
+        }
+      }
+    }
+
+    freeGames: games(
+      filters: { price: { eq: 0 } }
+      sort: "release_date:desc"
+      pagination: { limit: 8 }
+    ) {
+      data {
+        id
+        attributes {
+          ...GameFragment
+        }
+      }
+      meta {
+        pagination {
+          ...PaginationFragment
+        }
+      }
+    }
+
+    sections: home {
+      data {
+        id
+        attributes {
+          newGames {
+            id
+            title
+            highlight {
+              ...HighlightFragment
+            }
+          }
+
+          popularGames {
+            id
+            title
+            highlight {
+              ...HighlightFragment
+            }
+            games(pagination: { limit: 8 }) {
+              data {
+                id
+                attributes {
+                  ...GameFragment
+                }
               }
             }
           }
-          title
-          subtitle
-          button {
+
+          upcomingGames {
             id
-            label
-            link
+            title
+            highlight {
+              ...HighlightFragment
+            }
           }
-          ribbon {
+
+          freeGames {
             id
-            text
-            color
-            size
+            title
+            highlight {
+              ...HighlightFragment
+            }
           }
         }
       }
     }
   }
+
+  ${BannerFragment}
+  ${GameFragment}
+  ${PaginationFragment}
+  ${HighlightFragment}
 `;
