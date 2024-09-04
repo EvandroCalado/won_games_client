3970deb015fc7a4f8823fd04a8e1c4c0807a5693
Evandro Calado <evandrocalado07@gmail.com>
2024-09-04 20:27:36 -0300

    🦄 refactor: add free if value is zero

--- src/utils/format-price.ts
+++ src/utils/format-price.ts
@@ -1,4 +1,6 @@
 export const formatPrice = (price: number | bigint): string => {
+  if (!price) return 'FREE';
+
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
