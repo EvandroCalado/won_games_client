d15b52e16250fb444b6180410dfa1ecb7698f587
Evandro Calado <evandrocalado07@gmail.com>
2024-09-05 23:17:02 -0300

    🦄 refactor: mode uri for .env file

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -10,7 +10,7 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
     cache: new InMemoryCache(),
     link: new HttpLink({
       // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: 'http://localhost:1337/graphql',
+      uri: process.env.API_URI,
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
       // fetchOptions: { cache: "no-store" },
