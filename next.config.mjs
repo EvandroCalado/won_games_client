ab78f86a804c0503b14466902028bf87dfde69f4
Evandro Calado <evandrocalado07@gmail.com>
2024-09-08 20:22:12 -0300

    🦄 refactor: change domains to remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
