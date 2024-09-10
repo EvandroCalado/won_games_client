3f2722a93cd103c6363144b7dc9d8cf6d2e3df9c
Evandro Calado <evandrocalado07@gmail.com>
2024-09-10 19:42:25 -0300

    🐳 chore: add preload query

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,25 +1,14 @@
 import { mock as mockSidebar } from '@/components/ExploreSidebar/mock';
 import { QUERY_GAMES } from '@/graphql/queries';
-import { getClient } from '@/lib/client';
+import { PreloadQuery } from '@/lib/client';
 import { Games } from '@/templates';
 
 const GamesPage = async () => {
-  const { data } = await getClient().query({
-    query: QUERY_GAMES,
-    variables: { limit: 9 },
-  });
-
-  const games = data.games.data.map((game) => ({
-    id: game.id,
-    title: game.attributes.name,
-    slug: game.attributes.slug,
-    developer: game.attributes.developers.data[0].attributes.name,
-    img: game.attributes.cover.data?.attributes.formats.small.url,
-    price: game.attributes.price,
-    promotionalPrice: game.attributes.price * 0.75,
-  }));
-
-  return <Games games={games} filterItems={mockSidebar} />;
+  return (
+    <PreloadQuery query={QUERY_GAMES}>
+      <Games filterItems={mockSidebar} />
+    </PreloadQuery>
+  );
 };
 
 export default GamesPage;
