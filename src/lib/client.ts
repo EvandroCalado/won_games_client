ac523b095ae75f198a9af08e128f567fb228bef4
Evandro Calado <evandrocalado07@gmail.com>
2024-09-10 21:49:48 -0300

    🐳 chore: remove comments

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -9,11 +9,7 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      // this needs to be an absolute url, as relative urls cannot be used in SSR
       uri: process.env.API_URI,
-      // you can disable result caching here if you want to
-      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      // fetchOptions: { cache: "no-store" },
     }),
   });
 });
