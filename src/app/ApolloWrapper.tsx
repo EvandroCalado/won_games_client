9a935618b21e7e4dc0df23fcec5be52194a42cd8
Evandro Calado <evandrocalado07@gmail.com>
2024-09-10 21:49:21 -0300

    🐳 chore: add typepolicies

--- src/app/ApolloWrapper.tsx
+++ src/app/ApolloWrapper.tsx
@@ -9,26 +9,34 @@ import {
 
 function makeClient() {
   const httpLink = new HttpLink({
-    // this needs to be an absolute url, as relative urls cannot be used in SSR
     uri: process.env.NEXT_PUBLIC_API_URI,
-    // you can disable result caching here if you want to
-    // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
+
     fetchOptions: { cache: 'no-store' },
-    // you can override the default `fetchOptions` on a per query basis
-    // via the `context` property on the options passed as a second argument
-    // to an Apollo Client data fetching hook, e.g.:
-    // const { data } = useSuspenseQuery(MY_QUERY, { context: { fetchOptions: { cache: "force-cache" }}});
   });
 
-  // use the `ApolloClient` from "@apollo/experimental-nextjs-app-support"
   return new ApolloClient({
-    // use the `InMemoryCache` from "@apollo/experimental-nextjs-app-support"
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            games: {
+              keyArgs: false,
+              merge(existing, incoming) {
+                if (!existing) return incoming;
+                return {
+                  ...incoming,
+                  data: [...existing.data, ...incoming.data],
+                };
+              },
+            },
+          },
+        },
+      },
+    }),
     link: httpLink,
   });
 }
 
-// you need to create a component to wrap your app in
 export function ApolloWrapper({ children }: React.PropsWithChildren) {
   return (
     <ApolloNextAppProvider makeClient={makeClient}>
