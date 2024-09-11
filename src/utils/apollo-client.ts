374b794a72f9a1bfa600f3f977eb5cc792b520ad
Evandro Calado <evandrocalado07@gmail.com>
2024-09-11 15:22:06 -0300

    🐳 chore: move apollo client to utils

---
+++ src/utils/apollo-client.ts
@@ -0,0 +1,15 @@
+import { HttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  registerApolloClient,
+} from '@apollo/experimental-nextjs-app-support';
+
+export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+      uri: process.env.API_URI,
+    }),
+  });
+});
