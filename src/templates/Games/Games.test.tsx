a554e2e5a14439c934a24afe6ca3cdb4a4276ce1
Evandro Calado <evandrocalado07@gmail.com>
2024-09-17 19:36:55 -0300

    🦄 refactor: add graphql suport for tests

--- src/templates/Games/Games.test.tsx
+++ src/templates/Games/Games.test.tsx
@@ -1,82 +1,85 @@
-import { QUERY_GAMES } from '@/graphql/queries';
-import { MockedProvider, MockedResponse } from '@apollo/client/testing';
-import { render, screen } from '@testing-library/react';
+import { mock as mockSidebar } from '@/components/ExploreSidebar/mock';
+import { apolloCache } from '@/utils/apollo-cache';
+import { MockedProvider } from '@apollo/client/testing';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Suspense } from 'react';
 import { Games } from '.';
+import { fetchMoreMock, gamesMock } from './mocks';
 
-import { mock as mockSidebar } from '@/components/ExploreSidebar/mock';
+jest.mock('@/templates/Base', () => ({
+  __esModule: true,
+  Base: ({ children }: { children: React.ReactNode }) => (
+    <div data-testid="Mock Base">{children}</div>
+  ),
+}));
 
-const mocks: MockedResponse[] = [
-  {
-    request: {
-      query: QUERY_GAMES,
-    },
-    result: {
-      data: {
-        games: {
-          data: [
-            {
-              id: '1',
-              attributes: {
-                name: 'Sample Game',
-                slug: 'sample-game',
-                price: 50,
-                developers: {
-                  data: [{ attributes: { name: 'Sample Developer' } }],
-                },
-                cover: {
-                  data: {
-                    attributes: {
-                      formats: {
-                        small: {
-                          url: 'sample-url',
-                        },
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          ],
-        },
-      },
-    },
-  },
-];
-
-// jest.mock('@/templates/Base', () => ({
-//   __esModule: true,
-//   Base: ({ children }: { children: React.ReactNode }) => (
-//     <div data-testid="Mock Base">{children}</div>
-//   ),
-// }));
-
-// jest.mock('@/components/ExploreSidebar', () => ({
-//   __esModule: true,
-//   ExploreSidebar: () => <div data-testid="Mock ExploreSidebar" />,
-// }));
-
-// jest.mock('@/components/GameCard', () => ({
-//   __esModule: true,
-//   GameCard: () => <div data-testid="Mock GameCard" />,
-// }));
+jest.mock('@/components/ExploreSidebar', () => ({
+  __esModule: true,
+  ExploreSidebar: () => <div data-testid="Mock ExploreSidebar" />,
+}));
 
 describe('Games', () => {
-  it('should render sidebar, games and show more', () => {
+  it('should render loading', () => {
     render(
-      <MockedProvider mocks={mocks} addTypename={false}>
-        <Suspense fallback={<h1>Loading...</h1>}>
+      <MockedProvider mocks={[gamesMock]} addTypename={false}>
+        <Suspense fallback="Loading...">
           <Games filterItems={mockSidebar} />
         </Suspense>
       </MockedProvider>,
     );
 
-    // expect(screen.getByTestId('Mock Base')).toBeInTheDocument();
-    // expect(screen.getByTestId('Mock ExploreSidebar')).toBeInTheDocument();
-    // expect(screen.getAllByTestId('Mock GameCard')).toHaveLength(1);
-    // expect(
-    //   screen.getByRole('button', { name: /show more/i }),
-    // ).toBeInTheDocument();
     expect(screen.getByText(/loading.../i)).toBeInTheDocument();
+  });
+
+  it('should render sidebar, games and show more', async () => {
+    render(
+      <MockedProvider mocks={[gamesMock]} addTypename={false}>
+        <Suspense fallback="Loading...">
+          <Games filterItems={mockSidebar} />
+        </Suspense>
+      </MockedProvider>,
+    );
+
+    // loading
+    expect(screen.getByText(/loading.../i)).toBeInTheDocument();
+
+    // await render all component
+    expect(await screen.findByTestId('Mock Base')).toBeInTheDocument();
+    expect(
+      await screen.findByTestId('Mock ExploreSidebar'),
+    ).toBeInTheDocument();
+
+    expect(screen.getByText(/Hitman: Blood Money/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /show more/i }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render more games when click show more', async () => {
+    render(
+      <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
+        <Suspense fallback="Loading...">
+          <Games filterItems={mockSidebar} />
+        </Suspense>
+      </MockedProvider>,
+    );
+
+    // await render all component
+    expect(await screen.findByTestId('Mock Base')).toBeInTheDocument();
+    expect(
+      await screen.findByTestId('Mock ExploreSidebar'),
+    ).toBeInTheDocument();
+
+    expect(await screen.findByText(/Hitman: Blood Money/i)).toBeInTheDocument();
+
+    screen.logTestingPlaygroundURL();
+
+    // fetch more games
+    const button = screen.getByRole('button', { name: /show more/i });
+    fireEvent.click(button);
+
+    expect(await screen.findByText(/bloodborne/i)).toBeInTheDocument();
+
+    screen.logTestingPlaygroundURL();
   });
 });
